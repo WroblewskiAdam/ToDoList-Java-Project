d50cf577c1132835b6e54f219e6f43d6e31f6030
Yaroslav <garbar.jarek@gmail.com>
2022-04-02 21:10:52 +0200

    create block of team and userItem

---
+++ src/frontend/src/components/teamItem/TeamItem.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useState, useRef } from 'react';
+import "./TeamItem.scss"
+import logo from '../../images/picture/team.png'
+import useOuterClick from '../../hooks/useClickOutside';
+
+function TeamItem(props) {
+    const [option, setOption] = useState(false);
+    const optionRef = useRef();
+    const moreButtonRef = useRef()
+
+    const handleOptionClick = () => {
+        setOption((option) => !option);
+    }
+
+    const handleTeamItemClick = (e) => {
+        if(!optionRef.current.contains(e.target) && !moreButtonRef.current.contains(e.target))
+            console.log("Choose TeamItem: ", props.id);
+    }
+
+    const handleDeleteClick = () => {
+        console.log("Delete: ", props.id);
+    }
+
+    const handleEditClick = () => {
+        console.log("Edit: ", props.id);
+    }
+
+    
+    useOuterClick(optionRef, () => {
+        setOption(false);
+    });
+
+    return (
+        <div className='teamItem' onClick={handleTeamItemClick}>
+            <div className="teamItem__image">
+                <img src={logo} alt="" />
+            </div>
+            <div className="teamItem__title">
+                {props.title}
+            </div>
+            <div ref={moreButtonRef} className="teamItem__more" onClick={handleOptionClick}>
+                ...
+            </div>
+            <div ref={optionRef} className={"teamItem__options" + (option ? "" : " hide")}>               
+                <div className="teamItem__btn teamItem__edit" onClick={handleEditClick}>
+                    Edit
+                </div>
+                <div className="teamItem__btn teamItem__delete" onClick={handleDeleteClick}>
+                    Delete
+                </div>
+            </div>
+        </div>
+    );
+}
+export default TeamItem;
