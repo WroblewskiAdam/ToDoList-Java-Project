3548e8ea8f773b548e551e76fb9736c8519dbeae
Adam Dąbkowski <adamdobrus@gmail.com>
2022-05-27 12:31:21 +0200

    Add - yaml file configurator - loading initial data

--- src/main/java/com/example/PAP2022/MyBackendApplication.java
+++ src/main/java/com/example/PAP2022/MyBackendApplication.java
@@ -1,13 +1,21 @@
 package com.example.PAP2022;
 
+import com.example.PAP2022.yamlconfiguration.YamlConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBackendApplication {
+public class MyBackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		YamlConfig yamlConfig = new YamlConfig();
+//		yamlConfig.changeYamlProperties("create", "always"); - Initial properties
+		yamlConfig.changeYamlProperties("update", "never");
+	}
 }
