7b474c2c1f28cf36528d15867f529cf4691f9395
Adam Dąbkowski <adamdobrus@gmail.com>
2022-05-30 22:26:52 +0200

    Edit Tests

--- src/frontend/src/services/appUserService.js
+++ src/frontend/src/services/appUserService.js
@@ -25,7 +25,7 @@ const getTasks = async (userId=JSON.parse(localStorage.getItem("accessToken")).i
 
 
 const getImage = async (userId) => {
-    return await axios.get(API_URL + "/image", {headers: authHeader(), params: {id: userId}}).then(res => res).catch(error => console.log(error));
+    return await axios.get(API_URL + "/image", {headers: authHeader(), params: {id: userId}, responseType: "multipart"}).then(res => res).catch(error => console.log(error));
 }
 
 const editUser = async (firstName, lastName) => {
@@ -76,7 +76,7 @@ const changeUserRole = async (userId, role) => {
 }
 
 const AppUserService = {
-    getAllUsers, 
+    getAllUsers,
     getUser,
     getTeams,
     getTasks,
